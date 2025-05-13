1b0649cbd297015dcadd3eaa681b1b7d9f466198
Ana Ruiz Montejo <156439763+bagatunga@users.noreply.github.com>
2025-05-13 20:59:33 +0200

    Update funciones.js

--- ruiz-ana_Buscaminas/JS/funciones.js
+++ ruiz-ana_Buscaminas/JS/funciones.js
@@ -6,17 +6,17 @@ const buscaminas = {
     campoMinas: []
 }
 
-function pintarTablero(nFilas,nColumnas){
+function pintarTablero(nFilas, nColumnas) {
     const tablero = document.querySelector("#tablero");
-    tablero.style.gridTemplateColumns = "repeat("+nColumnas+",25px)";
-    tablero.style.gridTemplateRows = "repeat("+nFilas+",25px)";
+    tablero.style.gridTemplateColumns = "repeat(" + nColumnas + ",25px)";
+    tablero.style.gridTemplateRows = "repeat(" + nFilas + ",25px)";
 
-    for(let i=0;i<nColumnas;i++){
-        for(let j=0;j<nFilas;j++){
+    for (let i = 0; i < nColumnas; i++) {
+        for (let j = 0; j < nFilas; j++) {
             const casilla = document.createElement('div');
-            casilla.setAttribute("id","F" + i + "C" + j);
-            casilla.addEventListener("contextmenu",marcar);
-            casilla.addEventListener("click",destapar);
+            casilla.setAttribute("id", "F" + i + "C" + j);
+            casilla.addEventListener("contextmenu", marcar);
+            casilla.addEventListener("click", destapar);
             tablero.appendChild(casilla);
         }
     }
@@ -25,18 +25,16 @@ function pintarTablero(nFilas,nColumnas){
 function generarCampoMinas() {
     buscaminas.campoMinas = new Array(buscaminas.nFilas);
 
-    for (let filas = 0; filas < buscaminas.nFilas; filas++) {
-        buscaminas.campoMinas[filas] = new Array(buscaminas.nColumnas);
+    for (let i = 0; i < buscaminas.nFilas; i++) {
+        buscaminas.campoMinas[i] = new Array(buscaminas.nColumnas);
     }
 }
 
-function esparcirMinas(cantidadMinas) {
+function esparcirMinas() {
     let minas = 0;
-
     while (minas < buscaminas.nMinasTot) {
         let fila = Math.floor(Math.random() * buscaminas.nFilas);
         let columna = Math.floor(Math.random() * buscaminas.nColumnas);
-
 
         if (buscaminas.campoMinas[fila][columna] != "B") {
             buscaminas.campoMinas[fila][columna] = "B";
@@ -47,15 +45,11 @@ function esparcirMinas(cantidadMinas) {
 
 function minasAlrededor(fila, columna) {
     let nMinasAlrededor = 0;
-
     for (let i = fila - 1; i <= fila + 1; i++) {
-
         for (let j = columna - 1; j <= columna + 1; j++) {
-            if (i >= 0 && j >= 0 && i < buscaminas.nFilas && j < buscaminas.nColumnas) {
-                nMinasAlrededor++;
-
+            if (i >= 0 && i < buscaminas.nFilas && j >= 0 && j < buscaminas.nColumnas) {
                 if (buscaminas.campoMinas[i][j] == "B") {
-
+                    nMinasAlrededor++;
                 }
             }
         }
@@ -64,97 +58,163 @@ function minasAlrededor(fila, columna) {
 }
 
 function contarMinas() {
-    for (let i = 0; buscaminas.nFilas > i; i++) {
-        for (let j = 0; buscaminas.nColumnas > j; i++){
-            if (buscaminas.campoMinas[i][j] != "B"){
-                minasAlrededor(i, j)
+    for (let i = 0; i < buscaminas.nFilas; i++) {
+        for (let j = 0; j < buscaminas.nColumnas; j++) {
+            if (buscaminas.campoMinas[i][j] != "B") {
+                minasAlrededor(i, j);
             }
         }
     }
 }
 
-function destaparCasilla(fila, columna){
-    if (fila >= 0 && fila < buscaminas.nFilas && columna >= 0 && columna < buscaminas.nColumnas){
+function destaparCasilla(fila, columna) {
+    if (fila >= 0 && fila < buscaminas.nFilas && columna >= 0 && columna < buscaminas.nColumnas) {
         let casilla = document.getElementById("F" + fila + "C" + columna);
-        if (!casilla.classList.contains("destapado")){
-            if (!casilla.innerHTML.includes('<img class="bandera"')){
+        if (!casilla.classList.contains("destapado")) {
+            if (!casilla.innerHTML.includes('<img class="bandera"')) {
                 casilla.classList.add("destapado");
+
             }
-              casilla.innerHTML = buscaminas.campoMinas[fila][columna];
+            casilla.innerHTML = buscaminas.campoMinas[fila][columna];
+
             casilla.classList.add("c" + buscaminas.campoMinas[fila][columna])
 
-             if(buscaminas.campoMinas[fila][columna] !== "B"){
-                if(buscaminas.campoMinas[fila][columna] == 0){
-                    destaparCasilla(fila-1,columna-1);
-                    destaparCasilla(fila-1,columna);
-                    destaparCasilla(fila-1,columna+1);
-                    destaparCasilla(fila,columna-1);
-                    destaparCasilla(fila,columna);
-                    destaparCasilla(fila+1,columna-1);
-                    destaparCasilla(fila+1,columna);
-                    destaparCasilla(fila+1,columna+1);
-                    
+            if (buscaminas.campoMinas[fila][columna] !== "B") {
+                if (buscaminas.campoMinas[fila][columna] == 0) {
+                    destaparCasilla(fila - 1, columna - 1);
+                    destaparCasilla(fila - 1, columna);
+                    destaparCasilla(fila - 1, columna + 1);
+                    destaparCasilla(fila, columna - 1);
+                    destaparCasilla(fila, columna);
+                    destaparCasilla(fila + 1, columna - 1);
+                    destaparCasilla(fila + 1, columna);
+                    destaparCasilla(fila + 1, columna + 1);
+
                     casilla.innerHTML = "";
                 }
             } else {
                 casilla.innerHTML = '<img class="explosion" src="IMAGENES/icons/explosion.png">'
+
+                resolverTablero(false);
             }
+
         }
     }
 }
 
 function inicio() {
     let dificultad = 0;
-    do{
+    do {
         dificultad = prompt("Introduce la dificultad. 1:Fácil  2:Medio  3:Difícil");
         dificultad = Number(dificultad);
-    } while(isNaN(dificultad) || !Number.isInteger(dificultad) || dificultad < 1 || dificultad > 3);
-    
-    switch (dificultad){
+    } while (isNaN(dificultad) || !Number.isInteger(dificultad) || dificultad < 1 || dificultad > 3);
+
+    switch (dificultad) {
         case 1:
             buscaminas.nMinasTot = 10;
             buscaminas.nColumnas = 8;
             buscaminas.nFilas = 8;
-        break;
+            break;
         case 2:
             buscaminas.nMinasTot = 40;
             buscaminas.nColumnas = 16;
             buscaminas.nFilas = 16;
-        break;
+            break;
         case 3:
             buscaminas.nMinasTot = 99;
             buscaminas.nFilas = 16;
             buscaminas.nColumnas = 30;
-        break;
+            break;
     }
-    pintarTablero(buscaminas.nFilas,buscaminas.nColumnas);
+    pintarTablero(buscaminas.nFilas, buscaminas.nColumnas);
     generarCampoMinas();
     esparcirMinas();
     contarMinas();
 }
 
-function marcar(e){
-    e.preventDefault();
-    console.log("Has marcado la casilla " + e.target.id);
-}
+function resolverTablero(resultado) {
+    let casillas = document.querySelector('#tablero').children;
 
-function destapar(e){
-    console.log("Has destapado la casilla " + e.target.id);
+    for (let i = 0; i < casillas.length; i++) {
+        casillas[i].removeEventListener("click", destapar);
+        casillas[i].removeEventListener("contextmenu", marcar);
 
-    let casilla = e.currentTarget;
-    let fila;
-    let columna;
-     if(casilla.id[2] == "C"){
-        fila = casilla.id[1];
-        columna = casilla.id.slice(3);
-    } else {
-        fila = casilla.id.slice(1,3);
-        columna = casilla.id.slice(4);
+        let fila;
+        let columna;
+        if (casillas[i].id[2] == "C") {
+            fila = casillas[i].id[1];
+            columna = casillas[i].id.slice(3);
+        } else {
+            fila = casillas[i].id.slice(1, 3);
+            columna = casillas[i].id.slice(4);
+        }
+
+        fila = Number(fila);
+        columna = Number(columna);
+
+        if (casillas[i].innerHTML.includes('<img class="bandera"')) {
+            casillas[i].classList.add("destapado");
+            if (buscaminas.campoMinas[fila][columna] == "B") {
+                casillas[i].innerHTML = '<img class="bomba" src="IMAGENES/icons/bomba.png">';
+            } else {
+                casillas[i].innerHTML = '<img class="banderaMal" src="IMAGENES/icons/banderaMal.png">';
+                resultado = false;
+            }
+        } else if (!casillas[i].classList.contains("destapado")) {
+            if (buscaminas.campoMinas[fila][columna] == "B") {
+                casillas[i].classList.add("destapado");
+                casillas[i].innerHTML = '<img class="bomba" src="IMAGENES/icons/bomba.png">';
+            }
+        }
     }
-    fila = Number(fila);
-    columna = Number(columna);
 
-     destaparCasilla(fila, columna);
+    if (resultado) {
+        alert("FELICIDADES HAS GANADO!")
+
+    }
 }
 
-window.onload = inicio();
+
+    function marcar(e) {
+        e.preventDefault();
+        console.log("Has marcado la casilla " + e.target.id);
+
+        let casilla = e.currentTarget
+
+        if (casilla.innerHTML.includes('<img class="bandera')) {
+            casilla.innerHTML = '<img class="duda" src="IMAGENES/icons/interrogacion.png">'
+            buscaminas.nMinasEncontradas--;
+        } else if (casilla.innerHTML.includes('<img class="duda')) {
+            casilla.innerHTML = "";
+        } else {
+            casilla.innerHTML = '<img class="bandera" src="IMAGENES/icons/bandera.png">'
+            buscaminas.nMinasEncontradas++;
+        }
+
+        if (buscaminas.nMinasEncontradas == buscaminas.nMinasTot) {
+            resolverTablero(true);
+        }
+    }
+
+    function destapar(e) {
+        console.log("Has destapado la casilla " + e.target.id);
+
+        let casilla = e.currentTarget;
+
+        let fila;
+        let columna;
+        if (casilla.id[2] == "C") {
+            fila = casilla.id[1];
+            columna = casilla.id.slice(3);
+        } else {
+            fila = casilla.id.slice(1, 3);
+            columna = casilla.id.slice(4);
+        }
+
+        fila = Number(fila);
+        columna = Number(columna);
+
+        destaparCasilla(fila, columna);
+    }
+
+    window.onload = inicio();
